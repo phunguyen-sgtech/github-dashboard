be70adeb032ece6e05b980a1f78e87b96cb74889
STS-PHUNGUYEN\phu.nguyen <phu.nguyen@saigontechnology.com>
2020-08-28 09:43:06 +0700

    Updates

---
+++ precache-manifest.b1ca0210d38f031ed8fb333d8fa5dce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d17e7ff01aa90e6e3b77144e4d5a0fb",
+    "url": "/github-dashboard/index.html"
+  },
+  {
+    "revision": "6ff9a97044080ded2e15",
+    "url": "/github-dashboard/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a5306ccadaa5b6648e5c",
+    "url": "/github-dashboard/static/css/main.38abdea9.chunk.css"
+  },
+  {
+    "revision": "6ff9a97044080ded2e15",
+    "url": "/github-dashboard/static/js/2.0f45048c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github-dashboard/static/js/2.0f45048c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5306ccadaa5b6648e5c",
+    "url": "/github-dashboard/static/js/main.c7f8ce7e.chunk.js"
+  },
+  {
+    "revision": "ad6480f23577cdc51850",
+    "url": "/github-dashboard/static/js/runtime-main.cb2b332b.js"
+  }
+]);
